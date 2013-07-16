3461d345c9a4ebbd9772a2f6d2a8df6ba83f8d60
Chris Mear <chris@feedmechocolate.com>
2013-07-16 09:31:14 +0100

    Enable setting of merge_timeout when running 'push' command.
    Introduces a new environment variable, COVERALLS_MERGE_TIMEOUT. If
    this is set, the 'push' command will pass the given merge timeout
    setting to SimpleCov before requesting the coverage results.

--- lib/coveralls.rb
+++ lib/coveralls.rb
@@ -29,6 +29,9 @@ module Coveralls
 
   def push!
     require 'simplecov'
+    if merge_timeout
+      ::SimpleCov.merge_timeout(merge_timeout)
+    end
     result = ::SimpleCov::ResultMerger.merged_result
     Coveralls::SimpleCov::Formatter.new.format result
   end
@@ -94,5 +97,11 @@ module Coveralls
 
   def noisy?
     ENV["COVERALLS_NOISY"] || @noisy
+  end
+
+  def merge_timeout
+    merge_timeout = ENV["COVERALLS_MERGE_TIMEOUT"].to_i if ENV["COVERALLS_MERGE_TIMEOUT"]
+    merge_timeout ||= @merge_timeout
+    merge_timeout
   end
 end
